b5479cfca8c44efe0a1a95f48a3fece9f7211791
gdu-dev <minkt78@naver.com>
2023-02-10 12:22:13 +0900

    2023-02-10

--- Day07_02_서브쿼리(부서,사원).sql
+++ Day07_02_서브쿼리(부서,사원).sql
@@ -96,7 +96,7 @@ SELECT EMP_NO, NAME, DEPART, GENDER, POSITION, HIRE_DATE, SALARY
   FROM EMPLOYEE_TBL
  WHERE POSITION IN (SELECT POSITION
                       FROM EMPLOYEE_TBL
-                     WHERE DEPART = 2);  -- 서브쿼리의 WHERE절에서 사용한 칼럼이 PK나 UNIQUE 칼럼이 아니므로 다중 행 서브쿼리이다.
+                     WHERE DEPART = 2);  -- WHERE절에서 사용한 DEPART 칼럼이 PK/UNIQUE 칼럼이 아니므로 다중 행 서브쿼리이다.
                                          -- 따라서 단일 행 연산자인 등호(=) 대신 다중 행 연산자 IN을 사용해야 한다.
 
 
@@ -109,7 +109,7 @@ SELECT EMP_NO, NAME, DEPART, GENDER, POSITION, HIRE_DATE, SALARY
   FROM EMPLOYEE_TBL
  WHERE DEPART IN (SELECT DEPT_NO
                     FROM DEPARTMENT_TBL
-                   WHERE DEPT_NAME = '영업부');  -- WHERE절에서 사용한 DEPART_NAME 칼럼은 PK/UNIQUE가 아니므로 다중 행 서브쿼리이다.
+                   WHERE DEPT_NAME = '영업부');  -- WHERE절에서 사용한 DEPART_NAME 칼럼이 PK/UNIQUE가 아니므로 다중 행 서브쿼리이다.
 
 -- 참고. 조인으로 풀기
 SELECT E.EMP_NO, E.NAME, E.DEPART, E.GENDER, E.POSITION, E.HIRE_DATE, E.SALARY
@@ -117,6 +117,23 @@ SELECT E.EMP_NO, E.NAME, E.DEPART, E.GENDER, E.POSITION, E.HIRE_DATE, E.SALARY
     ON D.DEPT_NO = E.DEPART
  WHERE D.DEPT_NAME = '영업부';
 
+
+-- 8. 직급이 '과장'인 사원들이 근무하는 부서 정보를 조회하시오.
+SELECT 부서정보
+FROM 부서
+WHERE 부서번호 IN (직급이 '과장'인 사원들이 근무하는 부서번호);
+
+SELECT DEPT_NO, DEPT_NAME, LOCATION
+  FROM DEPARTMENT_TBL
+ WHERE DEPT_NO IN (SELECT DEPART
+                     FROM EMPLOYEE_TBL
+                    WHERE POSITION = '과장');  -- WHERE절에서 사용한 POSITION 칼럼이 PK/UNIQUE가 아니므로 다중 행 서브쿼리이다.
+
+-- 참고. 조인으로 풀기
+SELECT D.DEPT_NO, D.DEPT_NAME, D.LOCATION
+  FROM DEPARTMENT_TBL D INNER JOIN EMPLOYEE_TBL E
+    ON D.DEPT_NO = E.DEPART
+ WHERE E.POSITION = '과장';
 
 
 
