bbcea9b3741d475c1529778916141bfbbf8b9be5
gdu-dev <minkt78@naver.com>
2023-03-02 19:34:59 +0900

    2023-03-02

--- SQL응용_재평가.sql
+++ SQL응용_재평가.sql
@@ -96,9 +96,62 @@ COMMIT;
 -- 작성된 USER_PROC 프로시저를 호출하기 위한 코드도 함께 작성하시오.
 -- (참고. 1960년에 태어난 사람은 1명이고, 이름은 이경규이다.) (프로시저 작성 8점, 프로시저 호출 2점)
 
+-- 작성
+CREATE OR REPLACE PROCEDURE USER_PROC
+IS
+    NAME USERS.USER_NAME%TYPE;
+BEGIN
+    SELECT USER_NAME
+      INTO NAME
+      FROM USERS
+     WHERE USER_YEAR = 1960;
+    DBMS_OUTPUT.PUT_LINE(NAME);
+END;
 
--- 11. 사용자의 아이디(USER_ID)를 전달하면 해당 아이디를 가진 사용자의 구매총액(PROD_PRICE * BUY_AMOUNT)의 합계를 계산한 뒤 합계가 1000 이상이면 'A', 1000 미만 500 이상이면 'B', 500 미만이면 'C'를 반환하는 GET_GRADE() 사용자 함수를 작성하시오. 아이디가 'KHD'인 사용자의 이름(USER_NAME)과 GET_GRADE() 함수의 결과를 조회하는 쿼리문을 작성하시오. (함수 작성 8점, 함수 결과 조회 2점)
+-- 호출
+EXECUTE USER_PROC();
+
+
+-- 11. 사용자의 아이디(USER_ID)를 전달하면 해당 아이디를 가진 사용자의 구매총액(PROD_PRICE * BUY_AMOUNT)의 합계를 계산한 뒤 합계가 1000 이상이면 'A', 1000 미만 500 이상이면 'B', 500 미만이면 'C'를 반환하는 GET_GRADE() 사용자 함수를 작성하시오.
+--     아이디가 'KHD'인 사용자의 이름(USER_NAME)과 GET_GRADE() 함수의 결과를 조회하는 쿼리문을 작성하시오. (함수 작성 8점, 함수 결과 조회 2점)
+CREATE OR REPLACE FUNCTION GET_GRADE(USERID USERS.USER_ID%TYPE)
+RETURN VARCHAR2
+IS
+    BUY_TOTAL NUMBER;
+BEGIN
+    SELECT SUM(PROD_PRICE * BUY_AMOUNT)
+      INTO BUY_TOTAL
+      FROM BUYS
+     WHERE USER_ID = USERID;
+    IF BUY_TOTAL >= 1000 THEN
+        RETURN 'A';
+    ELSIF BUY_TOTAL >= 500 THEN
+        RETURN 'B';
+    ELSE
+        RETURN 'C';
+    END IF;
+END;
+
+SELECT USER_NAME AS 고객명, GET_GRADE('KHD') AS 등급
+  FROM USERS
+ WHERE USER_ID = 'KHD';
+
+-- 팁. 모든 고객의 등급 확인은?
+SELECT USER_NAME AS 고객명, GET_GRADE(USER_ID) AS 등급
+  FROM USERS;
 
 
 -- 12. 구매(BUYS) 테이블의 각 행(Row)이 수정(UPDATE)되거나 추가(INSERT)된 이후에 '구매내역이 변동되었습니다.'라는 서버메시지를 출력하는 BUYS_TRIG 트리거를 작성하시오. (10점)
+CREATE OR REPLACE TRIGGER BUYS_TRIG
+    AFTER
+    UPDATE OR INSERT
+    ON BUYS
+    FOR EACH ROW
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('구매내역이 변동되었습니다.');
+END;
 
+-- 팁. 트리거 동작 확인
+UPDATE BUYS
+   SET USER_ID = 'KHD'
+ WHERE BUY_NO = 1001;
